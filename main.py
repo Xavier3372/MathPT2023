ab21a86c70d9773e1c3f7def5d06cfaa2010ed0f
Xavier Koh <xavierkoh16@gmail.com>
2023-05-21 20:23:28 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,27 @@ import numpy as np
 
 class Graph(Scene):
     def construct(self):
-        axes = Axes(x_range=[-4, 5, 1], y_range=[0, 21, 1],x_length=10,y_length=5, axis_config = {"include_tip": False, "include_numbers" : False})
-        axis_labels = axes.get_axis_labels(x_label="x", y_label="y")
-        graph = axes.plot(lambda x : x**2, color = YELLOW, x_range=[-4,4])
-        equation = MathTex(r"y=x^2")
-        equation.next_to(axes, UP * 3)
+        axes1 = Axes(x_range=[-4, 5, 1], y_range=[0, 21, 1],x_length=5,y_length=3, axis_config = {"include_tip": False, "include_numbers" : False})
+        axis1_labels = axes1.get_axis_labels(x_label="x", y_label="y")
+        graph1 = axes1.plot(lambda x : x**2, color = YELLOW, x_range=[-4,4])
+        equation1 = MathTex(r"y=f(x)")
+        equation1.next_to(axes1, UP * 3)
+        graphgroup1 = VGroup(equation1, graph1, axis1_labels, axes1)
 
-        self.play(DrawBorderThenFill(axes), Write(axis_labels))
-        self.play(Write(equation))
-        self.play(Create(graph))
+        arrow = Arrow(start=LEFT, end=RIGHT)
+
+        axes2 = Axes(x_range=[-4, 5, 1], y_range=[0, 21, 1],x_length=5,y_length=3, axis_config = {"include_tip": False, "include_numbers" : False}).shift(RIGHT * 4)
+        axis2_labels = axes2.get_axis_labels(x_label="x", y_label="y")
+        graph2 = axes1.plot(lambda x : 2*(x**2)+3, color = YELLOW, x_range=[-3,3]).shift(RIGHT * 4)
+        equation2 = MathTex(r"y=af(bx+c)+d")
+        equation2.next_to(axes2, UP * 3)
+        graphgroup2 = VGroup(equation2, graph2, axis2_labels, axes2)
+
+        self.play(Write(equation1))
+        self.play(DrawBorderThenFill(axes1), Write(axis1_labels))
+        self.play(Create(graph1))
+        self.play(graphgroup1.animate.shift(LEFT*4), run_time = 3)
+        self.play(Write(arrow))
+        self.play(Write(equation2))
+        self.play(DrawBorderThenFill(axes2), Write(axis2_labels))
+        self.play(Create(graph2))
